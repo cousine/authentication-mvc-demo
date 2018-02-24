6014e01d0c7e71d82fb758f4561f3dc83ff96488
Omar Mekky <alphaengine@gmail.com>
2018-02-24 11:01:27 +0200

    Refactor user model into service object

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #  updated_at      :datetime         not null
 #
 
-require 'digest/SHA2'
+require 'secure_password'
 class User < ApplicationRecord
   attr_reader :password
   attr_accessor :password_confirmation
@@ -22,27 +22,7 @@ class User < ApplicationRecord
   def password=(value)
     @password = value
     self.password_seed = "#{Random.new.rand(1000)}#{Time.now.to_i}"
-    self.hashed_password = User.generate_password_hash(value, self.password_seed)
+    self.hashed_password = SecurePassword.generate_password_hash(value, self.password_seed)
   end
 
-  def self.authenticate(user_params)
-    email = user_params[:email]
-    password = user_params[:password]
-
-    user = User.find_by! email: email
-    password_to_check = User.generate_password_hash(password, user.password_seed)
-    if password_to_check == user.hashed_password
-      return user
-    end
-
-    raise ActiveRecord::RecordNotFound
-    
-  end
-
-  def self.generate_password_hash(password, password_seed)
-    secret = 'AlmakinahSecretPhrase'
-    password_to_hash = "#{password}#{secret}#{password_seed}"
-
-    Digest::SHA2.hexdigest(password_to_hash)
-  end
 end
---
+++ lib/secure_password.rb
@@ -0,0 +1,8 @@
+module SecurePassword
+  def self.generate_password_hash(password, password_seed)
+    secret = Rails.application.secrets.secret_key_base
+    password_to_hash = "#{password}#{secret}#{password_seed}"
+
+    Digest::SHA2.hexdigest(password_to_hash)
+  end
+end
